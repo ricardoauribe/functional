ce6450410281e1340812bd476285f9e87a99e265
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-05-16 18:10:03 -0500

    Chain methods

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// Durations are in minutes
- 
+//reference: https://code.tutsplus.com/es/tutorials/how-to-use-map-filter-reduce-in-javascript--cms-26209
+
+// Durations are in minutes 
 var tasks = [
   {
     'name'     : 'Test functional programming',
@@ -57,3 +58,50 @@ var total_time = tasks.reduce(function (acc, current) {
 }, 0);
 
 console.log(total_time)
+
+//Chain various actions
+
+var monday = [
+	{
+			'name'     : 'Write a tutorial',
+			'duration' : 180
+	},
+	{
+			'name'     : 'Some web development',
+			'duration' : 120
+	}
+];
+
+var tuesday = [
+	{
+			'name'     : 'Keep writing that tutorial',
+			'duration' : 240
+	},
+	{
+			'name'     : 'Some more web development',
+			'duration' : 180
+	},
+	{
+			'name'     : 'A whole lot of nothing',
+			'duration'  : 240
+	}
+];
+
+var tasks = [monday, tuesday];
+
+// Concatenate our 2D array into a single list
+var result = tasks.reduce((acc, current) => acc.concat(current))
+	// Extract the task duration, and convert minutes to hours
+	.map((task) => task.duration / 60)
+	// Filter out any task that took less than two hours
+	.filter((duration) => duration >= 2)
+	// Multiply each tasks' duration by our hourly rate
+	.map((duration) => duration * 25)
+	// Combine the sums into a single dollar amount
+	.reduce((acc, current) => [(+acc) + (+current)])
+	// Convert to a "pretty-printed" dollar amount
+	.map((amount) => '$' + amount.toFixed(2))
+	// Pull out the only element of the array we got from map
+	.reduce((formatted_amount) =>formatted_amount); 
+
+	console.log(result)
